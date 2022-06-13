1fa0ff8522858462d620a766f4322302b09f1ff8
Matthew Reddin <m.reddin@me.com>
2022-06-13 17:09:56 +0100

    Initial Commit

---
+++ Flights Screensaver/Flights_ScreensaverApp.swift
@@ -0,0 +1,17 @@
+//
+//  Flights_ScreensaverApp.swift
+//  Flights Screensaver
+//
+//  Created by Matthew Reddin on 13/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct Flights_ScreensaverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
